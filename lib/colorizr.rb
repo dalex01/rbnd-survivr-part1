30e51ee06deaf24d628f41fd153c8db079bc2322
Alexey Drozdov <alexey.drozdov@gmail.com>
2016-02-29 09:49:53 +0300

    Correct per review comments

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,15 +1,15 @@
 class String
 	attr_reader :colors_codes, :colors
 
-	@@colors_codes = {:red 				=> "\e[31m",
-			   		  			:green			=> "\e[32m",
-			   		  			:yellow			=> "\e[33m",
-			   		  			:blue				=> "\e[34m",
-			   		  			:pink				=> "\e[35m",
-			   		  			:light_blue	=> "\e[94m",
-			   		  			:white			=> "\e[97m",
-			   		  			:light_grey	=> "\e[37m",
-			   		  			:black			=> "\e[30m"
+	@@colors_codes = {red:        "\e[31m",
+			   		  			green:      "\e[32m",
+			   		  			yellow:     "\e[33m",
+			   		  			blue:       "\e[34m",
+			   		  			pink:       "\e[35m",
+			   		  			light_blue: "\e[94m",
+			   		  			white:      "\e[97m",
+			   		  			light_grey: "\e[37m",
+			   		  			black:      "\e[30m"
 			  		 }
 	def self.create_colors
 		@@colors_codes.each do |color, code|
@@ -21,25 +21,21 @@ class String
 
 	self.create_colors
 
-	String.class_eval {
-	  def self.color
-	    @@colors_codes.keys
-	  end
-	}
+	def self.color
+	  @@colors_codes.keys
+	end
 
-	String.class_eval {
-	  def self.sample_colors
-	    puts "Red text".red
-  		puts "Green text".green
-  		puts "Yellow text".yellow
-  		puts "Blue text".blue
-  		puts "Pink text".pink
-  		puts "Light blue text".light_blue
-  		puts "White text".white
-  		puts "Light grey text".light_grey
-  		puts "Black text".black
-	  end
-	}
+	def self.sample_colors
+	  puts "Red text".red
+	  puts "Green text".green
+	  puts "Yellow text".yellow
+	  puts "Blue text".blue
+	  puts "Pink text".pink
+	  puts "Light blue text".light_blue
+	  puts "White text".white
+		puts "Light grey text".light_grey
+		puts "Black text".black
+  end
 
 end
 
